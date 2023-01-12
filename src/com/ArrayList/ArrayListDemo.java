6a51f7618c75cdd2887bf289e03c4cde80e2df0e
snehal <snehaldahare@gmail.com>
2023-01-12 19:53:06 +0530

    first commit

---
+++ src/com/ArrayList/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package com.ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList arrayList=new ArrayList();
+		arrayList.add("snehal");
+		arrayList.add("Dahare");
+		arrayList.add("pune");
+		
+		System.out.println(arrayList);
+	}
+
+}
